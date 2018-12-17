e76b78ee1baa3809c9a7467dde5c6acc2a5d970b
RikiyaOta <rikiya0421.fcb6@gmail.com>
2018-12-17 17:04:34 +0900

    Modify a little.

--- task_seat/lib/task_seat/tasks/tasks.ex
+++ task_seat/lib/task_seat/tasks/tasks.ex
@@ -2,4 +2,14 @@ defmodule TaskSeat.Tasks do
   @moduledoc """
     This module provide public API for Tasks context
   """
+
+  alias TaskSeat.Repo
+  alias TaskSeat.Accounts.User
+
+  def get_sheats_of_user(%User{} = user) do
+    user
+    |> Repo.preload(:sheats)
+    |> Map.get(:sheats)
+  end
+
 end
--- task_seat/lib/task_seat_web/controllers/page_controller.ex
+++ task_seat/lib/task_seat_web/controllers/page_controller.ex
@@ -2,13 +2,22 @@ defmodule TaskSeatWeb.PageController do
   use TaskSeatWeb, :controller
 
   alias TaskSeat.Accounts.User.Guardian
+  alias TaskSeat.Tasks
 
   def home(conn, _params) do
     current_user = Guardian.Plug.current_resource(conn)
-    conn
-    |> assign(:conn, conn)
-    |> assign(:current_user, current_user)
-    |> render("home.html")
+    if is_nil(current_user) do
+      conn
+      |> assign(:current_user, nil)
+      |> render("home.html")
+    else
+      sheats_of_current_user = Tasks.get_sheats_of_user(current_user)
+      conn
+      |> assign(:conn, conn)
+      |> assign(:current_user, current_user)
+      |> assign(:sheats, sheats_of_current_user)
+      |> render("home.html")
+    end
   end
 
 end
