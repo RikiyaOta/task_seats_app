8bffdc5545a56ddd53e7bf6f4c03df4418525f6b
RikiyaOta <rikiya0421.fcb6@gmail.com>
2018-12-18 20:58:32 +0900

    When not-login user access pages required login, redirect to login page.

--- task_sheet/lib/task_sheet/accounts/auth/error_handler.ex
+++ task_sheet/lib/task_sheet/accounts/auth/error_handler.ex
@@ -1,11 +1,18 @@
 defmodule TaskSheet.Accounts.User.ErrorHandler do
   import Plug.Conn
+  import Phoenix.Controller, only: [put_flash: 3, redirect: 2]
 
   def auth_error(conn, {type, _reason}, _opts) do
     body = to_string(type)
+
+    #テキストで表示されるの味気ない
+    #conn
+    #|> put_resp_content_type("text/plain")
+    #|> send_resp(401, body)
+
     conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(401, body)
+    |> put_flash(:error, "ログインしてください")
+    |> redirect(to: TaskSheetWeb.Router.Helpers.task_sheet_session_path(conn, :new))
   end
 
 end
