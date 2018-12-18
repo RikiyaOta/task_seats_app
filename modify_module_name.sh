767c11caaf2388712da0ff82f3da7bcaacb7f705
RikiyaOta <rikiya0421.fcb6@gmail.com>
2018-12-18 10:52:40 +0900

    modify shell script.

--- modify_module_name.sh
+++ modify_module_name.sh
@@ -5,5 +5,5 @@ new_module_name="TaskSheet"
 
 for filename in $(grep -l -E $old_module_name task_seat/lib/**/*.*)
 do
-    sed -e "s/${old_module_name}/${new_module_name}/g" filename > ${filename}.tmp && cp ${filename}.tmp filename && rm ${filename}.tmp
+    sed -e "s/${old_module_name}/${new_module_name}/g" $filename > ${filename}.tmp && cp ${filename}.tmp $filename && rm ${filename}.tmp
 done
